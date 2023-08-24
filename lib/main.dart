62f84b2490b5d7c45cf8500ebfec5dc9e7aabde5
Hussain <hraza@vctech.io>
2023-08-24 15:33:42 +0500

    Added animals

--- lib/main.dart
+++ lib/main.dart
@@ -30,146 +30,341 @@ class ContainerPage extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final containerWidth = screenWidth / 2 - 20; // Subtract 20 for spacing
 
-    return
-
-      Container(
+    return Container(
       width: double.infinity, // Full width of the screen
       padding: EdgeInsets.symmetric(horizontal: 10),
-      child: Column(
-        children: [
-          SizedBox(height: 20,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  assetsAudioPlayer.open(
-                    Audio('assets/chickenSound2.mp3'), // Replace with your audio file path
-                  );
-                  print('Audio playing from Container 1');
-                },
-                child: Container(
-                  width: containerWidth,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image.asset('assets/Chicken.jpeg', height: 110, width: 110,),
-                        SizedBox(height: 10,),
-                        Text("CHICKEN", style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold
-                        ),)
-                      ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    assetsAudioPlayer.open(
+                      Audio('assets/chickenSound2.mp3'), // Replace with your audio file path
+                    );
+                    print('Audio playing from Container 1');
+                  },
+                  child: Container(
+                    width: containerWidth,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/Chicken.jpeg', height: 110, width: 110,),
+                          SizedBox(height: 10,),
+                          Text("CHICKEN", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(width: 20), // Add spacing between containers
-              GestureDetector(
-                onTap: () {
-                  assetsAudioPlayer.open(
-                    Audio('assets/catMeow.mp3'), // Replace with your audio file path
-                  );
-                  print('Audio playing from Container 1');
-                },
-                child: Container(
-                  width: containerWidth,
-                  height: 150,
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image.asset('assets/Cat.jpeg', height: 110, width: 110,),
-                        SizedBox(height: 10,),
-                        Text("CAT", style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold
-                        ),)
-                      ],
+                SizedBox(width: 20), // Add spacing between containers
+                GestureDetector(
+                  onTap: () {
+                    assetsAudioPlayer.open(
+                      Audio('assets/catMeow.mp3'), // Replace with your audio file path
+                    );
+                    print('Audio playing from Container 1');
+                  },
+                  child: Container(
+                    width: containerWidth,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/Cat.jpeg', height: 110, width: 110,),
+                          SizedBox(height: 10,),
+                          Text("CAT", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 20,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  assetsAudioPlayer.open(
-                    Audio('assets/lionSound.aac'), // Replace with your audio file path
-                  );
-                  print('Audio playing from Container 3');
-                },
-                child: Container(
-                  width: containerWidth,
-                  height: 150,
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image.asset('assets/Lion.jpeg', height: 110, width: 110,),
-                        SizedBox(height: 10,),
-                        Text("LION", style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold
-                        ),)
-                      ],
+              ],
+            ),
+            const SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    assetsAudioPlayer.open(
+                      Audio('assets/lionSound.aac'), // Replace with your audio file path
+                    );
+                    print('Audio playing from Container 3');
+                  },
+                  child: Container(
+                    width: containerWidth,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/Lion.jpeg', height: 110, width: 110,),
+                          SizedBox(height: 10,),
+                          Text("LION", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(width: 20), // Add spacing between containers
-              GestureDetector(
-                onTap: () {
-                  assetsAudioPlayer.open(
-                    Audio('assets/elephantSound.mpeg'), // Replace with your audio file path
-                  );
-                  print('Audio playing from Container 4');
-                },
-                child: Container(
-                  width: containerWidth,
-                  height: 150,
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image.asset('assets/elephant.jpeg', height: 110, width: 110,),
-                        SizedBox(height: 10,),
-                        Text("ELEPHANT", style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold
-                        ),)
-                      ],
+                SizedBox(width: 20), // Add spacing between containers
+                GestureDetector(
+                  onTap: () {
+                    assetsAudioPlayer.open(
+                      Audio('assets/elephantSound.mpeg'), // Replace with your audio file path
+                    );
+                    print('Audio playing from Container 4');
+                  },
+                  child: Container(
+                    width: containerWidth,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/elephant.jpeg', height: 110, width: 110,),
+                          SizedBox(height: 10,),
+                          Text("ELEPHANT", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          // ... (other code)
-        ],
-      ),
+              ],
+            ),
+            const SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    assetsAudioPlayer.open(
+                      Audio('assets/goat.mpeg'), // Replace with your audio file path
+                    );
+                    print('Audio playing from Container 3');
+                  },
+                  child: Container(
+                    width: containerWidth,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/goat.jpeg', height: 110, width: 110,),
+                          SizedBox(height: 10,),
+                          Text("GOAT", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20), // Add spacing between containers
+                GestureDetector(
+                  onTap: () {
+                    assetsAudioPlayer.open(
+                      Audio('assets/dog.aac'), // Replace with your audio file path
+                    );
+                    print('Audio playing from Container 4');
+                  },
+                  child: Container(
+                    width: containerWidth,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/dog.jpeg', height: 110, width: 110,),
+                          SizedBox(height: 10,),
+                          Text("DOG", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    assetsAudioPlayer.open(
+                      Audio('assets/Camel.mpeg'), // Replace with your audio file path
+                    );
+                    print('Audio playing from Container 3');
+                  },
+                  child: Container(
+                    width: containerWidth,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/Camel.jpeg', height: 110, width: 110,),
+                          SizedBox(height: 10,),
+                          Text("CAMEL", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20), // Add spacing between containers
+                GestureDetector(
+                  onTap: () {
+                    assetsAudioPlayer.open(
+                      Audio('assets/Duck.mpeg'), // Replace with your audio file path
+                    );
+                    print('Audio playing from Container 4');
+                  },
+                  child: Container(
+                    width: containerWidth,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/Duck.jpeg', height: 110, width: 110,),
+                          SizedBox(height: 10,),
+                          Text("DUCK", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    assetsAudioPlayer.open(
+                      Audio('assets/Horse.aac'), // Replace with your audio file path
+                    );
+                    print('Audio playing from Container 3');
+                  },
+                  child: Container(
+                    width: containerWidth,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/Horse.jpeg', height: 110, width: 110,),
+                          SizedBox(height: 10,),
+                          Text("Horse", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20), // Add spacing between containers
+                GestureDetector(
+                  onTap: () {
+                    assetsAudioPlayer.open(
+                      Audio('assets/Cow.aac'), // Replace with your audio file path
+                    );
+                    print('Audio playing from Container 4');
+                  },
+                  child: Container(
+                    width: containerWidth,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/Cow.jpeg', height: 110, width: 110,),
+                          SizedBox(height: 10,),
+                          Text("COW", style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold
+                          ),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            // ... (other code)
+          ],
+        ),
+      )
     );
   }
 }
